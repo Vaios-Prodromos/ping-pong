0f976d1fc905fec07deaf98704e6acc18cdd8b84
Vaios-Prodromos <123881341+Vaios-Prodromos@users.noreply.github.com>
2024-07-09 12:30:24 +0300

    Update shooter_game.py

--- shooter_game.py
+++ shooter_game.py
@@ -15,15 +15,20 @@ mixer.music.play()
 fire_sound = mixer.Sound('fire.ogg')
 
 #we need the following images:
-img_back = "galaxy.jpg" #game background
-img_bullet = "bullet.png" #bullet
-img_hero = "rocket.png" #hero
-img_enemy = "ufo.png" #enemy
-score = 0 #ships destroyed
-goal = 10 #how many ships need to be shot down to win
-lost = 0 #ships missed
-max_lost = 3 #lose if you miss that many
-#parent class for other sprites
+img_player = "racket.png" #hero
+img_ball = "tennis_ball.png"
+img_back = "court.jpg"
+
+
+
+win_width = 700
+win_height = 500
+display.set_caption("Ping-Pong")
+window = display.set_mode((win_width, win_height))
+background = transform.scale(image.load(img_back), (win_width, win_height))
+window.blit(background,(0,0))
+
+
 class GameSprite(sprite.Sprite):
  #class constructor
    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
@@ -47,76 +52,28 @@ class Player(GameSprite):
    #method to control the sprite with arrow keys
    def update(self):
        keys = key.get_pressed()
-       if keys[K_LEFT] and self.rect.x > 5:
+       if keys[K_UP] and self.rect.x > 5:
            self.rect.x -= self.speed
-       if keys[K_RIGHT] and self.rect.x < win_width - 80:
+       if keys[K_DOWN] and self.rect.x < win_width - 80:
            self.rect.x += self.speed
  #method to "shoot" (use the player position to create a bullet there)
-   def fire(self):
-       bullet = Bullet(img_bullet, self.rect.centerx, self.rect.top, 15, 20, -15)
-       bullets.add(bullet)
+speed_x = 3
+speed_y = 3
+while game:
+ #...
+    if finish != True:
+        ball.rect
 
-#enemy sprite class  
-class Enemy(GameSprite):
-   #enemy movement
-   def update(self):
-       self.rect.y += self.speed
-       global lost
-       #disappears upon reaching the screen edge
-       if self.rect.y > win_height:
-           self.rect.x = randint(80, win_width - 80)
-           self.rect.y = 0
-           lost = lost + 1
-#bullet sprite class  
-class Bullet(GameSprite):
-   # enemy movement
-   def update(self):
-       self.rect.y += self.speed
-       # disappears upon reaching the screen edge
-       if self.rect.y < 0:
-           self.kill()
-#create a small window
-win_width = 700
-win_height = 500
-display.set_caption("Shooter")
-window = display.set_mode((win_width, win_height))
-background = transform.scale(image.load(img_back), (win_width, win_height))
-#create sprites
-ship = Player(img_hero, 5, win_height - 100, 80, 100, 10)
-#creating a group of enemy sprites
-monsters = sprite.Group()
-for i in range(1, 6):
-   monster = Enemy(img_enemy, randint(80, win_width - 80), -40, 80, 50, randint(1, 5))
-   monsters.add(monster)
-bullets = sprite.Group()
-#the "game is over" variable: as soon as True is there, sprites stop working in the main loop
-finish = False
-#main game loop:
-run = True #the flag is reset by the window close button
-while run:
-   #"Close" button press event
-   for e in event.get():
-       if e.type == QUIT:
-           run = False
-       #event of pressing the spacebar - the sprite shoots
-       elif e.type == KEYDOWN:
-           if e.key == K_SPACE:
-               fire_sound.play()
-               ship.fire()
- #the game itself: actions of sprites, checking the rules of the game, redrawing
    if not finish:
        #update the background
        window.blit(background,(0,0))
 
        #launch sprite movements
        ship.update()
-       monsters.update()
-       bullets.update()
 
        #update them in a new location in each loop iteration
-       ship.reset()
-       monsters.draw(window)
-       bullets.draw(window)
+       racket.reset()
+    
        #check for a collision between a bullet and monsters (both monster and bullet disappear upon a touch)
        collides = sprite.groupcollide(monsters, bullets, True, True)
        for c in collides:
